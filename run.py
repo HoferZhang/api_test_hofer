186b97ee6abbd70f74b1bdd2ef2d8c4f3bee2d8f
HoferZhang <mrhongfeng@163.com>
2018-04-25 18:33:05 +0800

    almost

--- run.py
+++ run.py
@@ -6,9 +6,11 @@ import json
 import sys
 import xlrd
 import xlwt
+from xlutils.copy import copy
 
 
 CaseFile = "TestCase/case.xlsx"
+ResultFile = "Result.xls"
 
 
 def get_case(test_case_file, case_no):
@@ -35,24 +37,61 @@ def get_sum_case(test_case_file):
     return sheet.nrows
 
 
-def run_request():
-    url = get_case(CaseFile, 1)[2]
-    data = get_case(CaseFile, 1)[4]
-    req_data = json.loads(data)
-    print(req_data)
-    # data = {'questionId':'59c333037f1008310d8b4567'}
+def run_request(caseId):
+    case = get_case(CaseFile, caseId)
+    print(type(case))
+
+    url = case[2]
+    els_data = case[4]
+    data_json = json.loads(els_data)
+
     header = {"Wxid": "o79aixECshqXft8Cck5fMC7LdYZs",
               "Channel": "wx_anxinjiankang",
               "User-Agent": "micromessenger"}
 
-    print(url, data)
-
-    rsp = requests.post(url=url, data=req_data, headers=header)
+    rsp = requests.post(url=url, data=data_json, headers=header)
     rsp_json = json.loads(rsp.text)
+    code = rsp_json["code"]
+    case.append(code)
 
-    print (rsp.url)
-    print(rsp.text)
-    print(rsp.json())
+    if code == case[5]:
+        case.append("pass")
+    else:
+        case.append("fail")
+
+    case.append(rsp_json)
+    print(case)
+
+    save_to_file(case, ResultFile)
 
 
-run_request()
+def save_to_file(rowlist, savefilename):
+    old_excel = xlrd.open_workbook(ResultFile, formatting_info=True)
+    old_sheet = old_excel.sheet_by_index(0)
+    row = old_sheet.nrows
+
+    new_excel = copy(old_excel)
+    new_sheet = new_excel.get_sheet(0)
+
+    for i in range(len(rowlist)):
+        new_sheet.write(row+1, i, rowlist[i])
+
+    new_excel.save(savefilename)
+
+
+def new_xls(filename, title):
+    w = xlwt.Workbook()
+    ws = w.add_sheet(u'Sheet1')
+
+    for i in range(len(title)):
+        ws.write(0, i, title[i])
+
+    w.save(filename)
+
+
+result_title = ["id", "Description", "Request_URL", "Method", "Request_Data", "Check_Point", "ActualResult",
+                    "TestResult", "Response"]
+new_xls(ResultFile, result_title)
+
+run_request(1)
+
