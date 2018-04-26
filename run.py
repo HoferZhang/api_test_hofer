4c23faf07c7a22fa79614f538876286fee728b69
HoferZhang <mrhongfeng@163.com>
2018-04-26 16:34:47 +0800

    finish

--- run.py
+++ run.py
@@ -3,43 +3,37 @@
 import requests
 import json
 
-import sys
+
+import time
 import xlrd
 import xlwt
 from xlutils.copy import copy
 
-
 CaseFile = "TestCase/case.xlsx"
-ResultFile = "Result.xls"
+ResultTitleRow = \
+    ["id", "Description", "Request_URL", "Method", "Request_Data", "ExpectedRspCode", "ActualRspCode", "TestResult",
+     "RspMeessage"]
+CurrentTime = time.strftime('%Y%m%d_%H%M%S', time.localtime(time.time()))
+ResultFile = "Result_" + CurrentTime + ".xls"
 
 
 def get_case(test_case_file, case_no):
     testCase = xlrd.open_workbook(test_case_file)
     sheet = testCase.sheet_by_index(0)
 
-    case_id = int(sheet.cell_value(case_no, 0))
+    case_id = sheet.cell_value(case_no, 0)
     description = sheet.cell_value(case_no, 1)
-
     request_url = sheet.cell_value(case_no, 2)
     request_method = sheet.cell_value(case_no, 3)
     request_data = sheet.cell_value(case_no, 4)
-
     check_point = sheet.cell_value(case_no, 5)
 
     case = [case_id, description, request_url, request_method, request_data, check_point]
     return case
 
 
-def get_sum_case(test_case_file):
-    testCase = xlrd.open_workbook(test_case_file)
-    sheet = testCase.sheet_by_index(0)
-
-    return sheet.nrows
-
-
-def run_request(caseId):
-    case = get_case(CaseFile, caseId)
-    print(type(case))
+def run_request(caseId, case, result):
+    case = get_case(case, caseId)
 
     url = case[2]
     els_data = case[4]
@@ -56,25 +50,25 @@ def run_request(caseId):
 
     if code == case[5]:
         case.append("pass")
+        case.append("null")
     else:
         case.append("fail")
+        case.append(rsp_json["msg"])
 
-    case.append(rsp_json)
-    print(case)
-
-    save_to_file(case, ResultFile)
+    save_to_file(case, result)
 
 
 def save_to_file(rowlist, savefilename):
     old_excel = xlrd.open_workbook(ResultFile, formatting_info=True)
     old_sheet = old_excel.sheet_by_index(0)
     row = old_sheet.nrows
+    print(row)
 
     new_excel = copy(old_excel)
     new_sheet = new_excel.get_sheet(0)
 
     for i in range(len(rowlist)):
-        new_sheet.write(row+1, i, rowlist[i])
+        new_sheet.write(row, i, rowlist[i])
 
     new_excel.save(savefilename)
 
@@ -89,9 +83,18 @@ def new_xls(filename, title):
     w.save(filename)
 
 
-result_title = ["id", "Description", "Request_URL", "Method", "Request_Data", "Check_Point", "ActualResult",
-                    "TestResult", "Response"]
-new_xls(ResultFile, result_title)
+def get_total_row(basic_excel):
+    excel = xlrd.open_workbook(basic_excel)
+    sheet = excel.sheet_by_index(0)
+    return sheet.nrows
 
-run_request(1)
 
+def run(case, result, result_title):
+    new_xls(result, result_title)
+    for i in range(get_total_row(case) - 1):
+        run_request(i + 1, case, result)
+
+
+# run_request(1, CaseFile, ResultFile)
+# run_request(2, CaseFile, ResultFile)
+run(CaseFile, ResultFile, ResultTitleRow)
--- test.py
+++ test.py
@@ -3,13 +3,7 @@
 import requests
 import json
 
-url = 'http://test.anxinyisheng.com/home/question/detailQuestion'
-params = {'questionId': '59c333037f1008310d8b4567'}
-header = {
-    "Wxid": "o79aixECshqXft8Cck5fMC7LdYZs",
-    "Channel": "wx_anxinjiankang",
-    "User-Agent": "micromessenger",
-    "Auth": "57320a1b2a8e8cdfa121b417bd0ef547"}
 
-rsp = requests.post(url, params=params, headers=header)
-print(rsp)
+a = [1, 2, 3]
+for i in range(len(a)):
+    print(i)
